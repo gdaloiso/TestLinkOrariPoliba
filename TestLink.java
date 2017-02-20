0d734bfdb788a12708bd6007b19df479f0e87b54
Giuseppe Daloiso <g.daloiso389@gmail.com>
2017-02-20 14:13:41 +0100

    fix

--- TestLink.java
+++ TestLink.java
@@ -81,7 +81,7 @@ public class TestLink
                 }
 				catch (IOException e)
 				{
-                    e.printStackTrace()
+                    e.printStackTrace();
                 }
 
             }
