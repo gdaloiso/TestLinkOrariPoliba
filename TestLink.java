13ca7ef1fe7669d343e94519f04a15340449b894
Giuseppe Daloiso <g.daloiso389@gmail.com>
2017-02-20 13:53:15 +0100

    test new link

--- TestLink.java
+++ TestLink.java
@@ -1,73 +1,121 @@
-import java.io.*;
-import java.net.*;
-import java.util.*;
-import java.nio.channels.Channels;
-import java.nio.channels.ReadableByteChannel;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
 
-public class TestLink {
-	private static Properties prop;
-	static int errors = 0;
-	public static void  main(String[] argv)
-	{
-		TestLink test = new TestLink();
-		prop = readCredentials();
-		Enumeration em = prop.keys();
-  		while(em.hasMoreElements())
-		{
-  			String str = (String)em.nextElement();
-			boolean result = test.downloadFile((String)prop.get(str));
-			if(!result)
-			{
-				System.out.println("Link al corso "+ str + " errato");
+
+public class TestLink
+{
+
+    
+    public static ArrayList<String> arrayCorsi = new ArrayList<>();
+    public static ArrayList<String> arrayLink1 = new ArrayList<>();
+    public static ArrayList<String> arrayLink2 = new ArrayList<>();
+    public static int errors = 0;
+    public static void main(String[] args)
+    {
+        TestLink test = new TestLink();
+        test.getResouceFromAssetFile();
+        for (int i=0; i< arrayCorsi.size(); i++)
+        {
+            boolean result1 = test.downloadFile(arrayLink1.get(i));
+            if(!result1)
+            {
+				System.out.println("Link al corso "+ arrayCorsi.get(i) + "_I errato");
 				errors = errors + 1;
-			}
-  		}
-		System.out.println("Finish with "+ errors + " errors");
-		System.exit(errors);
-	}
-	
-	public static Properties readCredentials()
+            }
+            /*boolean result2 = downloadFile(arrayLink2.get(i));
+            if(!result2)
+            {
+				System.out.println("Link al corso "+ arrayCorsi.get(i) + "_II errato");
+				errors = errors + 1;
+            }*/
+        }
+        System.out.println("Finish with "+ errors + " errors");
+        System.exit(errors);
+        
+    }
+    
+    
+    public static void getResouceFromAssetFile()
 	{
-		Properties prop = new Properties();
-		try
+        BufferedReader reader = null;
+        try
 		{
-			InputStream input = new FileInputStream("config.propertiesI");
-			prop.load(input);
+            InputStream input = new FileInputStream(new File("link.txt"));
+            reader = new BufferedReader( new InputStreamReader(input,"UTF-8"));
+
+            String mLine;
+            while ((mLine = reader.readLine()) != null)
+			{
+                String[] infoCorso = mLine.split(",");
+                String mNome = infoCorso[0];
+                String mLink1 = infoCorso[1];
+                String mLink2 = infoCorso[2];
+
+                //arrayCorsi.add(mNome.replaceAll("\\s",""));
+                arrayCorsi.add(mNome);
+                arrayLink1.add(mLink1.replaceAll("\\s",""));
+                arrayLink2.add(mLink2.replaceAll("\\s",""));
+            }
+
+        }
+		catch(IOException e)
+		{
+            e.printStackTrace();
+        }
+		finally
+		{
+            if (reader != null)
+			{
+                try
+				{
+                   reader.close();
+                }
+				catch (IOException e)
+				{
+                    e.printStackTrace()
+                }
+
+            }
+        }
+    }
+    
+    
+    public static  boolean downloadFile(String fileUrl)
+    {
+       	try {
+        	URL url = new URL(fileUrl);
+        	HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        	int code = urlConnection.getResponseCode();
+			if(code==200)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		catch (FileNotFoundException e)
+		{
+			e.printStackTrace();
+		}
+		catch (MalformedURLException e)
+		{
+			e.printStackTrace();
 		}
 		catch (IOException e)
 		{
 			e.printStackTrace();
 		}
-		return prop;
-	}
-
-	public static  boolean downloadFile(String fileUrl)
-	{
-        	try {
-			URL url = new URL(fileUrl);
-            		HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-            		int code = urlConnection.getResponseCode();
-		    	if(code==200)
-			{
-                		return true;
-            		}
-			else
-			{
-               			return false;
-            		}
-		}
-		catch (FileNotFoundException e)
-		{
-           		e.printStackTrace();
-        	}
-		catch (MalformedURLException e)
-		{
-            		e.printStackTrace();
-        	}
-		catch (IOException e)
-		{
-            		e.printStackTrace();
-        	}
 		return false;
-    	}
+    }
 }
